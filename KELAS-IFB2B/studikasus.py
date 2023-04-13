e4bb86f6d27b220565c882d679bbb46806939a64
nasruddinmhny <nasruddin.mhny@gmail.com>
2023-04-13 11:39:24 +0800

    130423

--- KELAS-IFB2B/studikasus.py
+++ KELAS-IFB2B/studikasus.py
@@ -1,4 +1,3 @@
-
 # menu 
 print('===== MENU MAKANAN =====')
 print('1. Nasi goreng \n\t1.1 nasi goreng seafood \n\t1.2 nasi goreng mawut')
@@ -8,6 +7,8 @@ keterang = ''
 ketMenu = ''
 pilihMenu = ''
 harga = 0
+totalBayar = 0
+kemabalian = 0
 
 menu = input('Silahkan Pilih Menu = ')
 
@@ -16,10 +17,13 @@ if menu == 'NASI GORENG':
     print('1. nasi goreng seafood 2. nasi goreng mawut')
 
     pilihMenu = input('Pilih Nasi Goreng Apa = ')
+    Jumlah = int(input('Mau Berapa Porsi = '))
     if pilihMenu == 'NASI GORENG SEAFOOD':
          harga = 30000
+         totalBayar = Jumlah * harga
     elif pilihMenu == 'NASI GORENG MAWUT':
          harga = 25000
+         totalBayar = Jumlah * harga
     else:
          ketMenu = 'Nasi Goreng Yang Anda Mau tidak ada'
 
@@ -28,21 +32,28 @@ elif menu == 'BAKSO':
     print('1.Bakso Popye 2.Bakso Beranak ')
 
     pilihMenu = input('Pilih Bakso Apa = ')
+    Jumlah = int(input('Mau Berapa Porsi = '))
     if pilihMenu == 'BAKSO POPY':
          harga = 20000
+         totalBayar = Jumlah * harga
     elif pilihMenu == 'BAKSO BERANAK':
          harga = 25000
+         totalBayar = Jumlah * harga
     else:
          ketMenu = 'Bakso Yang Anda Mau tidak ada'
 else:
      ketMenu = menu, 'Tidak Tersedia'
 
+print('Total Bayar = Rp. ',totalBayar)
+uangTunai = int(input('Terima Uang Tunai = Rp. '))
+kemabalian = uangTunai - totalBayar
+
 print('=== Output ===')
 print('Menu = ',menu)
 print('Pilih Menu = ',pilihMenu)
 print('Harga = Rp. ',harga)
+print('Jumlah Porsi = ',Jumlah)
+print('Total Bayar = Rp. ',totalBayar)
+print('Terima Uang = Rp. ',uangTunai)
+print('Kembalian = Rp. ',kemabalian)
 print('Keterangan Menu = ',ketMenu)
-
-
-
-
